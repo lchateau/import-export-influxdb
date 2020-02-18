c785df56103695c4132e6c1337b371054b7f19e5
lchateau <lucas.chateau@edu.univ-fcomte.fr>
2020-02-18 22:23:31 +0100

    Create export function that writes in .csv

--- export.py
+++ export.py
@@ -2,6 +2,7 @@ from influxdb import InfluxDBClient
 
 CONFIG_FILE_NAME = "config.env"
 db_name = "test"
+measurement = "test_temperature"
 
 with open(CONFIG_FILE_NAME, "r") as config_file:
     args = config_file.readline().split(":")
@@ -17,23 +18,42 @@ client.switch_database(db_name)
 start_date = "'2019-01-01'"
 end_date = "'2020-03-01'"
 
-request = "SELECT * FROM test_temperature WHERE time >= " + \
+
+def export(start_date, end_date, measurement):
+    request = "SELECT * FROM " + measurement + " WHERE time >= " + \
     start_date + " AND time <= " + end_date
 
-result = client.query(request)
-column_names = result.raw["series"][0]["columns"]
-csv_header = ""
+    result = client.query(request)
+    column_names = result.raw["series"][0]["columns"]
+    csv_header = ""
 
-for column in column_names:
-    csv_header += column + ","
-
-csv_header = csv_header[:-1]
-print(csv_header)
-pt = result.get_points()
-
-for r in pt:
-    line = ""
     for column in column_names:
-        line += str(r[column]) + ","
-    line = line[:-1]
-    print(line)
+        csv_header += column + ","
+
+    csv_header = csv_header[:-1]
+    
+    points = pt = result.get_points()
+
+    with open("export.csv", "w") as export_file:
+        export_file.write(csv_header + "\n")
+        for point in points:
+            line = ""
+            for column in column_names:
+                line += str(point[column]) + ","
+            line = line[:-1]
+            export_file.write(line + "\n")
+
+
+export(start_date, end_date, measurement)
+
+# json_body = [
+#     {
+#         "measurement": "test_temperature",
+#         "time": "2020-02-14T18:22:06.232589412Z",
+#         "fields": {
+#             "lon": 7.22
+#         }
+#     }
+# ]
+
+# # client.write_points(json_body)
