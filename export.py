1091e1fe19b3fafb9735cdb43e938142e122dedd
lchateau <lucas.chateau@edu.univ-fcomte.fr>
2020-02-23 15:01:51 +0100

    Changed separator from "," to "#"

--- export.py
+++ export.py
@@ -1,5 +1,6 @@
 from influxdb import InfluxDBClient
 
+SEPARATOR = "#"
 CONFIG_FILE_NAME = "config.env"
 db_name = "test"
 measurement = "test_temperature"
@@ -21,17 +22,17 @@ end_date = "'2020-03-01'"
 
 def export(start_date, end_date, measurement):
     request = "SELECT * FROM " + measurement + " WHERE time >= " + \
-    start_date + " AND time <= " + end_date
+        start_date + " AND time <= " + end_date
 
     result = client.query(request)
     column_names = result.raw["series"][0]["columns"]
     csv_header = ""
 
     for column in column_names:
-        csv_header += column + ","
+        csv_header += column + SEPARATOR
 
     csv_header = csv_header[:-1]
-    
+
     points = pt = result.get_points()
 
     with open("export.csv", "w") as export_file:
@@ -39,21 +40,10 @@ def export(start_date, end_date, measurement):
         for point in points:
             line = ""
             for column in column_names:
-                line += str(point[column]) + ","
+                line += str(point[column]) + SEPARATOR
             line = line[:-1]
             export_file.write(line + "\n")
 
 
 export(start_date, end_date, measurement)
 
-# json_body = [
-#     {
-#         "measurement": "test_temperature",
-#         "time": "2020-02-14T18:22:06.232589412Z",
-#         "fields": {
-#             "lon": 7.22
-#         }
-#     }
-# ]
-
-# # client.write_points(json_body)
